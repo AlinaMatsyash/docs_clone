1686f48fa02833f25e48febe16edc41fd1a3abe0
AlinaMatsyash <alina.mathyahs@gmail.com>
2022-12-14 18:31:46 +0400

    add google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:docs_clone/screens/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +15,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: LoginScreen(),
+    );
   }
 }
